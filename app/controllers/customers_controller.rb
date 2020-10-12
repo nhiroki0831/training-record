56b2e82594a888e9ae74da978a16086e1a663d96
nhiroki0831 <nhiroki0831@gmail.com>
2020-10-12 18:40:56 +0900

    顧客登録機能　単体テスト実施"
    This reverts commit ffe7e7fb42c664f0c52dccec5392dc4f146ed176.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,4 @@
 class CustomersController < ApplicationController
-  before_action :move_to_login
-
 
   def index
   end
@@ -23,9 +21,4 @@ class CustomersController < ApplicationController
     params.require(:customer).permit(:image,:first_name, :family_name, :first_name_k, :family_name_k, :gender, :birthday, :tel, :postal_code, :prefecture_id, :city, :address, :building, :job_id, :course_id, :height).merge(user_id: current_user.id)
   end
 
-  def move_to_login
-    unless user_signed_in?
-      redirect_to new_user_registration_path
-    end
-  end
 end
