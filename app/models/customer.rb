c308cc5848ed1224ffbcb500b6119885bc61ffbd
nhiroki0831 <nhiroki0831@gmail.com>
2020-10-13 19:01:49 +0900

    単体テストコード修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,14 +13,15 @@ class Customer < ApplicationRecord
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' } 
     validates :first_name_k, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
     validates :family_name_k, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
-    validates :gender
     validates :birthday
     validates :tel, format: { with: /\A\d{10}$|^\d{11}\z/, message: 'はハイフン(-)を使用せず数字のみ入力して下さい'}
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を使用してください'}
     validates :city
     validates :address
-    validates :height, numericality: { with: [/\A[0-9]+\z/ ,message: 'は半角数字のみ使用してください'], greater_than_or_equal_to: 100, less_than_or_equal_to: 220 }
+    validates :height, numericality: { with: /\A[0-9]+\z/ ,message: 'は半角数字のみ使用してください'}
   end
-  
-  validates :prefecture_id,:job_id,:course_id, numericality: { ohter_than: 1}
+    validates :height, numericality: {greater_than_or_equal_to: 100, less_than_or_equal_to: 220 }
+    validates :gender, presence: {message: 'を選択してください'} 
+    validates :prefecture_id, :job_id, :course_id, numericality: { other_than: 1, message: 'を選択してください' }
+ 
 end
