fa6262d8752856df25f2a5986b1e95dd204d4e5a
nhiroki0831 <nhiroki0831@gmail.com>
2020-10-17 15:37:03 +0900

    トレーニング情報記録用ビュー仮実装

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,6 @@
 class TrainingsController < ApplicationController
+
+  def index
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "customers#index"
   resources :customers do
-    resources :trainings, only: :new
+    resources :trainings, only: :index
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
