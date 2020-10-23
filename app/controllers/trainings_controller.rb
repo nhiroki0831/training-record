66409ee67a60fc4493c1d6307154d79370254f0c
nhiroki0831 <nhiroki0831@gmail.com>
2020-10-23 11:25:16 +0900

    トレーニング記録　編集機能実装

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -14,10 +14,20 @@ class TrainingsController < ApplicationController
     end  
   end
 
+  def edit
+    @customer = Customer.find(params[:customer_id])
+    @training = Training.find(params[:id])
+  end
+
+  def update
+    training = Training.find(params[:id])
+    training.update(training_params)
+  end
+
   private
 
   def training_params
-    params.permit(:weight, :body_fat, :blood_pressure_max, :blood_pressure_min, :pulse, :remarks, part:[]).merge(customer_id: params[:customer_id])
+    params.require(:training).permit(:weight, :body_fat, :blood_pressure_max, :blood_pressure_min, :pulse, :remarks, part:[]).merge(customer_id: params[:customer_id])
   end
 
 end
